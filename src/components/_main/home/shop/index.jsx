4c528ce1c83ec379bd647a1db33e9a8a74d20c8b
Kamran Haider <mohsindawood58@gmail.com>
2024-06-03 12:03:19 +0500

    fixed my small bug

--- src/components/_main/home/shop/index.jsx
+++ src/components/_main/home/shop/index.jsx
@@ -7,7 +7,7 @@ import { useSelector } from 'react-redux';
 // mui
 import { Typography, Grid, Box, Stack, Paper, Button } from '@mui/material';
 // icons
-import { IoIosArrowForward } from 'react-icons/io';s
+import { IoIosArrowForward } from 'react-icons/io';
 // component
 import ShopCard from 'src/components/cards/shop';
 
