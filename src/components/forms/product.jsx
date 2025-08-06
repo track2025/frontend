83df40bc4572c5cde756d5f70b6e51d0bbc9b4cd
James Ishola <brain@shoutty.app>
2025-08-06 20:00:54 +0400

    no message

--- src/components/forms/product.jsx
+++ src/components/forms/product.jsx
@@ -218,7 +218,7 @@ export default function ProductForm({
       ctx.rotate(angle);
 
       // 6. Better watermark positioning
-      const horizontalSpacing = canvas.width * 0.1;
+      const horizontalSpacing = canvas.width * 0.5;
       const verticalSpacing = canvas.height * 0.2;
 
       // 7. Draw watermark pattern
