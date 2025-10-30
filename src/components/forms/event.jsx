d56dd7e5ee8b6297689e5fc3c2f2a6d9b61a847d
shahadath-shoutty <a.shahadath@shoutty.app>
2025-10-30 14:41:53 +0600

    finished country code

--- src/components/forms/event.jsx
+++ src/components/forms/event.jsx
@@ -44,6 +44,7 @@ import uploadToSpaces from 'src/utils/upload';
 const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 import 'react-quill/dist/quill.snow.css';
 import parseMongooseError from 'src/utils/errorHandler';
+import countries from 'src/utils/counties';
 
 EventForm.propTypes = {
   data: PropTypes.object,
@@ -100,7 +101,7 @@ export default function EventForm({ data: currentEvent, isLoading: apiLoading })
     date: Yup.string().required('Date is required'),
     startTime: Yup.string().required('Start Time is required'),
     endTime: Yup.string().required('End Time is required'),
-    category: Yup.string().required('Category is required'),
+    category: Yup.string().optional('Category is required'),
     image: Yup.mixed().required('Main image is required'),
     thumbnailImage: Yup.mixed().required('Thumbnail image is required'),
     content: Yup.string().required('Content is required')
@@ -116,6 +117,7 @@ export default function EventForm({ data: currentEvent, isLoading: apiLoading })
       trackSlug: currentEvent?.trackSlug || '',
       country: currentEvent?.country || '',
       countrySlug: currentEvent?.countrySlug || '',
+      countryCode: currentEvent?.countryCode || '',
       city: currentEvent?.city || '',
       date: currentEvent?.date || '',
       startTime: currentEvent?.startTime || '',
@@ -312,12 +314,24 @@ export default function EventForm({ data: currentEvent, isLoading: apiLoading })
                         )}
                       />
 
-                      <TextField
-                        label="Country"
-                        {...getFieldProps('country')}
-                        placeholder=""
-                        error={Boolean(touched.country && errors.country)}
-                        helperText={touched.country && errors.country}
+                      <Autocomplete
+                        options={countries}
+                        getOptionLabel={(option) => option.label}
+                        value={countries?.find((c) => c.label === values.country) || null}
+                        onChange={(event, newValue) => {
+                          setFieldValue('country', newValue ? newValue.label : '');
+                          setFieldValue('countrySlug', newValue ? newValue.code.toLowerCase() : '');
+                          setFieldValue('countryCode', newValue ? newValue.code : '');
+                        }}
+                        renderInput={(params) => (
+                          <TextField
+                            {...params}
+                            label="Country"
+                            placeholder="Select a country"
+                            error={Boolean(touched.country && errors.country)}
+                            helperText={touched.country && errors.country}
+                          />
+                        )}
                       />
                       <TextField
                         label="City"
