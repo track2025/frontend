0a9c042650822e827209d20e0753906defe829f7
DESKTOP-ID4Q3QV\Hanniel <dhanniel21@gmail.com>
2025-11-13 11:12:58 +0100

    removed text

--- src/components/_main/track-products/Banner.jsx
+++ src/components/_main/track-products/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div
-            className="position-relative d-flex align-items-center justify-content-left text-white"
+            className="position-relative d-flex align-items-center text-white"
             style={{
                 height: '400px',
                 backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)), url("images/store-banner.jpg")',
@@ -12,10 +12,9 @@ const Banner = () => {
                 backgroundRepeat: 'no-repeat'
             }}
         >
-            <div className="text-center md:text-left  position-relative z-1 px-5 ">
+            <div className="text-center text-md-start px-5">
                 <h1 className="display-5 fw-bold mb-3">Precision. Protection. Performance.</h1>
                 <p className="fs-5 mb-2">Built for racers, trusted worldwide.</p>
-                <p className="fs-5 mb-2">But we still have Gain the competitive edge</p>
             </div>
         </div>
     );
