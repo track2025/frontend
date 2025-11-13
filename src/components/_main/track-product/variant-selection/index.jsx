29866525be5d6a48a3fbeef8e3a49d43d5987672
GabrielDelight <gabrieldelight08@gmail.com>
2025-11-13 10:30:33 +0100

    Resolved conflict

--- src/components/_main/track-product/variant-selection/index.jsx
+++ src/components/_main/track-product/variant-selection/index.jsx
@@ -14,17 +14,12 @@ export default function PhysicalProductVariantSelection({
   console.log("Variation Names", names);
 
   return (
-<<<<<<< HEAD
-    <div style={{ width: '100%', display: 'flex'}}>
-=======
     // <div style={{ width: '95%' }}>
     <Box width={'95%'}>
->>>>>>> 2025/11/11/changes-from-docs
       {names.map((name, index) => (
         <div style={{ width: '100%', display: 'grid'}}>
           <Typography variant="subtitle2" color="text.secondary" style={{ textTransform: "uppercase", fontWeight: 'bolder' }}>
             {name}
-<<<<<<< HEAD
           </Typography>
   
           <select
@@ -50,45 +45,6 @@ export default function PhysicalProductVariantSelection({
             ))}
           </select>
         </div>
-=======
-          </Typography> */}
-
-          {names.map((name, index) => (
-            <>
-              <Typography
-                variant="subtitle2"
-                color="text.secondary"
-                style={{ textTransform: 'uppercase', fontWeight: 'bolder' }}
-              >
-                {name}
-              </Typography>
-
-              <select
-                value={(variants[index] || []).find((v) => selectedVariant?.split('/')?.includes(v)) || ''}
-                onChange={(e) => onChangeVariant(e.target.value, index)}
-                style={{
-                  width: '95%',
-                  padding: '10px',
-                  borderRadius: '8px',
-                  border: '1px solid #ccc',
-                  outline: 'none',
-                  fontSize: '14px',
-                  textTransform: 'uppercase',
-                  backgroundColor: '#f4f4f4',
-                  borderWidth: 0,
-                  outline: 'none'
-                }}
-              >
-                {[...new Set(variants[index] || [])].map((variant, ind) => (
-                  <option key={variant} value={variant} style={{ textTransform: 'capitalize', fontWeight: 'bolder' }}>
-                    {variant.charAt(0).toUpperCase() + variant.slice(1)}
-                  </option>
-                ))}
-              </select>
-            </>
-          ))}
-        </Fragment>
->>>>>>> 2025/11/11/changes-from-docs
       ))}
     </Box>
   );
